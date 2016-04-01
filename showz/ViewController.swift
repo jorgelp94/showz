a7ec7494e75d9c94df9e4839835be7a5d3218de1
Jorge Luis Perales <jorgelp94@MacBook-Pro-de-Jorge.local>
2016-03-31 21:18:13 -0600

    project creation

---
+++ showz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  showz
+//
+//  Created by Jorge Luis Perales on 31/03/16.
+//  Copyright © 2016 Jorge Luis Perales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
